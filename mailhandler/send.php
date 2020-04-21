bd798ea26e34a966102cdbd91d260d53397fb7bf
Chandika Lasiru <54856322+clasiru@users.noreply.github.com>
2020-04-21 12:08:39 +0530

    Update send.php

--- mailhandler/send.php
+++ mailhandler/send.php
@@ -34,17 +34,18 @@ $lt= '<';
 $gt= '>';
 $sp= ' ';
 $from= 'From:';
-			$headers = $from.$fromname.$sp.$lt.$fromemail.$gt;
+$headers = $from.$fromname.$sp.$lt.$fromemail.$gt;
 
-			$oldphpself = $_SERVER['PHP_SELF'];
-			$oldremoteaddr = $_SERVER['REMOTE_ADDR'];
-			$_SERVER['PHP_SELF'] = "/";
-			$_SERVER['REMOTE_ADDR'] = $_SERVER['SERVER_ADDR'];
+$oldphpself = $_SERVER['PHP_SELF'];
+$oldremoteaddr = $_SERVER['REMOTE_ADDR'];
+$_SERVER['PHP_SELF'] = "/";
+$_SERVER['REMOTE_ADDR'] = $_SERVER['SERVER_ADDR'];
 
 
-			mail($to,$subject,$message,$headers);
+mail($to,$subject,$message,$headers);
 
-			$_SERVER['PHP_SELF'] = $oldphpself;
-			$_SERVER['REMOTE_ADDR'] = $oldremoteaddr;
-			exit();
+$_SERVER['PHP_SELF'] = $oldphpself;
+$_SERVER['REMOTE_ADDR'] = $oldremoteaddr;
+exit();
+
 ?>
